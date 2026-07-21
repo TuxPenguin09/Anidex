9d834978520003cff5d8db6e96664712f4b3a179
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2026-07-22 06:39:55 +0800

    Initial commit

---
+++ GachaView/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace GachaView.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ GachaView/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace GachaView.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
